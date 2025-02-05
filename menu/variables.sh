b2bd53465a216820e17955196f01dad0985e0fd3
abdielmoreno1 <240300737@ucaribe.edu.mx>
2025-02-05 08:42:27 -0500

    Tarea #995_Menú con ASCIINEMA

--- menu/variables.sh
+++ menu/variables.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
-
-A=""
-echo -n "Ingresa una cadena para imprimirla: "
-read A
-
-echo "$A"
+#!/bin/sh
+# Muestra el uso de variables
+# No existen los tipos
+# definición de variables
+una_variable=666
+MI_NOMBRE="Richard"
+NOMBRES="Iñigo Asier Sten Roberto Pello"
+BOOLEANO=true
+echo "Echemos un ojo a las variables "
+echo "Un número: ${una_variable}"
+echo "Un nombre ${MI_NOMBRE}"
+echo "Un grupo de nombres: ${NOMBRES}"
+# Al script se le pueden pasar argumentos. Para recogerlos
+# hay que usar : ${número} donde:
+# ${0} : nombre del script
+# ${1} : primer argumento
+# ${2} : segundo argumento
+# ...etc.
+echo "Has invocado el script pasándome ${0} eta ${1} "
